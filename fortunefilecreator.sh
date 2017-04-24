613910650ea626a01264543ccd4bdeeea1d54e30
root <root@somnus.grex>
2017-04-24 19:38:50 +0200

    WHARRGARBL

--- fortunefilecreator.sh
+++ fortunefilecreator.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# yomama
-cat $1 | perl -p -e 's/^\n/%/g' | sed 's/^%/%\n/g' > $2
+#yomama
+printf "\Takes a text file with blocks of text separated by whole blank lines and turns it into a %-delimited fortune file and a corresponding dat file.\n"
+printf "\nUsage: $0 inputfile outputfile\n";
+printf "\nExample 1: "
+printf "$0 brc.txt bobross \n";
+printf "Output: bobross bobross.dat \n";
 
+cat $1 | perl -p -e 's/^\n/%/g' | sed 's/^%/%\n/g' > $2
+strfile $2 $2.dat
